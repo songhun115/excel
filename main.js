aa23f3079c8c79f66484d3464561f6e192ab8fb4
songhun115 <hoho77113@daum.net>
2020-12-01 14:30:19 +0900

    헤더 및 슬라이더 마무리

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.nav_bar_item').hover(function(){
+    $('.nav_bar_item, .nav_right, .menu_toggle_btn').hover(function(){
       $('.nav_bar').addClass('on');
     }  , function(){
       $('.nav_bar').removeClass('on');
@@ -7,16 +7,39 @@ $(document).ready(function(){
    
 
 
-    $('.menu_toggle').click(function(){
+    $('.menu_toggle_btn').click(function(){
         const $clickd = $(this);
         if( $clickd.hasClass('open')) {
           $clickd.removeClass('open');
+          $('.nav_bar').removeClass('active');
           $('.all_menu').removeClass('open');
+          $('body').removeClass('menu_open');
         } else {
           $clickd.addClass('open');
+          $('.nav_bar').addClass('active');
           $('.all_menu').addClass('open');
+          $('body').addClass('menu_open');
         }
         })
        
+    $('.all_menu_item_h2').click(function(){
+      var submenu = $(this).next("ul");
+      if( submenu.is(":visible") ){
+          submenu.slideUp(300);
+      }else{
+          submenu.slideDown(300);
+      }
+     
 
   });
+
+});
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});
