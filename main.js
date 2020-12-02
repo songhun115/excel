6763cde99d80fe169babd46e47d683dd0e5aaafb
songhun115 <hoho77113@daum.net>
2020-12-02 17:23:54 +0900

    풀페이지 js 적용

--- main.js
+++ main.js
@@ -29,17 +29,26 @@ $(document).ready(function(){
       }else{
           submenu.slideDown(300);
       }
-     
-
   });
 
 });
+
 var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
+      return '<span class="' + className + '">' + '</span>';
     },
   },
+  
 });
+
+
+
